70811f18e080e51750d3b1cd7c801b26e66a9047
Fedor Paretsky <me@f3d0r.com>
2018-08-19 19:07:04 -0700

    Add timeout to start

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const gbfs = require('@gbfs');
 const skip = require('@skip');
 const spin = require('@spin');
 
-setInterval(lime.update, constants.general.update_interval);
-setInterval(gbfs.update, constants.general.update_interval);
-setInterval(skip.update, constants.general.update_interval);
-setInterval(spin.update, constants.general.update_interval);
+setTimeout(function () {
+    setInterval(lime.update, constants.general.update_interval);
+}, 0);
+
+setTimeout(function () {
+    setInterval(skip.update, constants.general.update_interval);
+}, 2500);
+
+setTimeout(function () {
+    setInterval(spin.update, constants.general.update_interval);
+}, 5000);
+
+setTimeout(function () {
+    setInterval(gbfs.update, constants.general.update_interval);
+}, 7500);
+
+
+
+
