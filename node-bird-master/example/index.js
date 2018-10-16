1eb61bcbe2af4d97a1c125c1deb37f2a44bf2a2a
Fedor Paretsky <me@f3d0r.com>
2018-10-16 13:48:44 -0700

    Restore index.js script

--- node-bird-master/example/index.js
+++ node-bird-master/example/index.js
@@ -1,75 +1,35 @@
-var Bird = require('..')
+const Bird = require('../')
 const bird = new Bird();
-var cloudscraper = require('cloudscraper');
-var readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let RATE_LIMIT_BASE = 100; //100ms separation
 
-function init(email) {
+const prompt = require("prompt-async");
+
+async function init() {
     try {
-        var options = {
-            method: 'POST',
-            url: 'https://api.birdapp.com/user/login',
-            headers: {
-                'content-type': 'application/json',
-                'app-version': '3.0.5',
-                'device-id': '1',
-                platform: 'ios'
-            },
-            body: {
-                email: email
-            },
-            json: true
-        };
-
-        cloudscraper.request(options, function (err, response, body) {
-            if (err) {
-                console.log(JSON.stringify(err));
-            } else {
-                response = JSON.stringify(response) + "";
-                var begString = "\"id\":\"";
-                var endString = "\",\"expires_at\"";
-                var id = response.substring(response.indexOf(begString) + begString.length, response.indexOf(endString));
-                if (id.indexOf("statusCode") == -1) {
-                    console.log(id);
-                    rl.question('Please enter the verifyCode: ', (verifyCode) => {
-                        rl.close();
-                        bird.verifyEmail(verifyCode)
-                            .then(function (response) {
-                                console.log(response);
-                                var reqs = [];
-                                for (var index = 1; index <= 1; index++) {
-                                    reqs.push(bird.getScootersNearby(45.512794, -122.679565, 5000));
-                                }
-                                Promise.all(reqs)
-                                    .then(function (responses) {
-                                        var index = 0
-                                        responses.forEach(function (response) {
-                                            console.log(response.length + "\t" + index++);
-                                        });
-                                    })
-                                    .catch(function (error) {
-                                        throw error;
-                                    });
-                            }).catch(function (error) {
-                                throw error;
-                            });
-                    });
-                } else {
-                    throw Error("Unable to login!");
-                }
-            }
-        });
+        await bird.login('me@f3d0r.com')
+        prompt.start();
+        const {
+            verifyCode
+        } = await prompt.get(['verifyCode']);
+        var email = await bird.verifyEmail(verifyCode)
+        var reqs = [];
+        for (var index = 1; index <= 300; index++) {
+            reqs.push(bird.getScootersNearby(45.512794, -122.679565, 5000));
+        }
+        Promise.all(reqs)
+            .then(function (responses) {
+                var index = 0
+                responses.forEach(function (response) {
+                    console.log(response.length + "\t" + index++);
+                });
+            })
+            .catch(function (error) {
+                throw error;
+            })
     } catch (err) {
-        console.log("ERROR: ");
         console.log(err)
     }
 }
 
-
-rl.question('Please enter your email: ', (email) => {
-    init(email);
-});
+init();
