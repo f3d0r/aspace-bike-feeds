d4f3176620a0b06200bbc766991db1b2ebba7670
Fedor Paretsky <me@f3d0r.com>
2018-10-19 22:36:30 -0700

    Update bird logs

--- apps/bird.js
+++ apps/bird.js
@@ -84,7 +84,7 @@ async function reloadScooters() {
         console.log("BIRD SCOOTERS || VERIFYING TOKEN...");
         var auth = await misc.performRequest(requestOptions.verifyOptions(loginToken, deviceId));
         authToken = auth.token;
-        console.log("BIRD SCOOTERS || AUTH TOKEN = " + authToken);
+        console.log("BIRD SCOOTERS || AUTH TOKEN VALID? " + authToken);
     }
 
     var reqs = [];
