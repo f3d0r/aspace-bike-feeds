56672798c2eb01dfbaa4817c0b7571d328cfa042
Fedor Paretsky <me@f3d0r.com>
2018-10-21 13:58:45 -0700

    Add aspace-davis database

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ module.exports = {
         DATABASE_USER: 'api',
         DATABASE_PASSWORD: 'db_password',
         DATABASE_NAME: 'aspace',
-        DATABASE_IP: 'localhost',
         DATABASE_PORT: 'db_port'
     },
     DATABASE_IPS: ['142.93.26.41',
         '159.65.103.1',
         '159.65.77.184',
         '206.189.216.212',
-        'db_ip'
+        'db_ip',
+        '35.185.229.112'
     ],
 }
