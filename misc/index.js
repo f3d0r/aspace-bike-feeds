f17021fe29e9557db17be0ab61d801265724ee80
Fedor Paretsky <me@f3d0r.com>
2018-10-21 22:40:31 -0700

    Add cloudscraper to bypass cloudflare block

--- misc/index.js
+++ misc/index.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var cloudscraper = require('cloudscraper');
+var sql = require('@sql');
 
 module.exports = {
     sleep: function (ms) {
@@ -11,7 +13,7 @@ module.exports = {
     performRequest: function (requestOptions) {
         return new Promise(function (resolve, reject) {
             requestOptions.headers['User-Agent'] = "insomnia/6.0.2";
-            request(requestOptions, function (error, response, body) {
+            cloudscraper.request(requestOptions, function (error, response, body) {
                 if (error) {
                     reject(error);
                 } else {
@@ -19,5 +21,33 @@ module.exports = {
                 }
             });
         });
+    },
+    getComparePromises: function (databaseName, lngKeyName, latKeyName, addObjectsKeys, systemName, compareResults, transportType = "bike", batteryLevelExists = false) {
+        var toRemoveQueries = "";
+        compareResults.idsToRemove.forEach(function (current) {
+            toRemoveQueries += `DELETE FROM \`${databaseName}\` WHERE \`id\` = '${current.id}'; `;
+        });
+        var removePromise = sql.runRaw(toRemoveQueries);
+
+        formattedObjects = [];
+        compareResults.idsToAdd.forEach(function (current) {
+            formattedObjects.push([systemName, 'US', current.bike_id, current.name, 1, transportType, current.lat, current.lon, current.jump_ebike_battery_level]);
+        });
+        var addPromise = sql.addObjects(databaseName, addObjectsKeys, formattedObjects);
+
+        toUpdateQueries = "";
+        compareResults.idsToUpdate.forEach(function (current) {
+            toUpdateQueries += `UPDATE \`${databaseName}\` SET \`lat\`='${current[latKeyName]}', \`lng\`='${current[lngKeyName]}' WHERE \`id\`='${current.id}'; `
+        });
+        var updatePromise = sql.runRaw(toUpdateQueries);
+
+        return {
+            removePromise,
+            addPromise,
+            updatePromise
+        }
+    },
+    getGBFSPromises: function () {
+
     }
 }
